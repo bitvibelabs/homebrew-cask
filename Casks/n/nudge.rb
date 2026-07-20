16b65b032870e43654622556122126bf45d9d927
Mike McQuaid <mike@mikemcquaid.com>
2026-07-20 21:08:24 +0100

    nudge: migrate command wrappers.

--- Casks/n/nudge.rb
+++ Casks/n/nudge.rb
@@ -15,16 +15,11 @@ cask "nudge" do
   depends_on macos: :monterey
 
   pkg "Nudge-#{version}.pkg"
-  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
-  shimscript = "#{staged_path}/nudge.wrapper.sh"
-  binary shimscript, target: "nudge"
 
-  preflight do
-    File.write shimscript, <<~EOS
-      #!/bin/sh
-      exec '/Applications/Utilities/Nudge.app/Contents/MacOS/Nudge' "$@"
-    EOS
-  end
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+
+  command_wrapper "nudge.wrapper.sh",
+                  executable: "/Applications/Utilities/Nudge.app/Contents/MacOS/Nudge"
 
   uninstall pkgutil: "com.github.macadmins.Nudge"
 
