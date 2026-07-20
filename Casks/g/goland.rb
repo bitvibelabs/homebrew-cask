6b838f5e141936fa275f70b3852c187d8bda5b41
Mike McQuaid <mike@mikemcquaid.com>
2026-07-20 21:08:23 +0100

    goland: migrate command wrappers.

--- Casks/g/goland.rb
+++ Casks/g/goland.rb
@@ -27,16 +27,12 @@ cask "goland" do
   depends_on :macos
 
   app "GoLand.app"
-  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
-  shimscript = "#{staged_path}/goland.wrapper.sh"
-  binary shimscript, target: "goland"
 
-  preflight do
-    File.write shimscript, <<~EOS
-      #!/bin/sh
-      open -na "GoLand.app" --args "$@"
-    EOS
-  end
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+
+  command_wrapper "goland.wrapper.sh",
+                  executable: "/usr/bin/open",
+                  args:       ["-na", "GoLand.app", "--args"]
 
   zap trash: [
     "~/Library/Application Support/JetBrains/GoLand",
