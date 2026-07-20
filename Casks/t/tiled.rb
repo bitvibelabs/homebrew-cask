bd080b444354cccce4af25e2453b905f6892f51b
Mike McQuaid <mike@mikemcquaid.com>
2026-07-20 21:08:25 +0100

    tiled: migrate command wrappers.

--- Casks/t/tiled.rb
+++ Casks/t/tiled.rb
@@ -30,16 +30,11 @@ cask "tiled" do
   depends_on :macos
 
   app "Tiled.app"
-  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
-  shimscript = "#{staged_path}/tiled.wrapper.sh"
-  binary shimscript, target: "tiled"
 
-  preflight do
-    File.write shimscript, <<~EOS
-      #!/bin/bash
-      exec '#{appdir}/Tiled.app/Contents/MacOS/Tiled' "$@"
-    EOS
-  end
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+
+  command_wrapper "tiled.wrapper.sh",
+                  executable: "#{appdir}/Tiled.app/Contents/MacOS/Tiled"
 
   zap trash: [
     "~/Library/Application Support/Tiled",
