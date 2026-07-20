ce409f9db69fa399a74591e2df821b837cd2587d
Mike McQuaid <mike@mikemcquaid.com>
2026-07-20 21:08:23 +0100

    gitfiend: migrate command wrappers.

--- Casks/g/gitfiend.rb
+++ Casks/g/gitfiend.rb
@@ -22,16 +22,11 @@ cask "gitfiend" do
   depends_on :macos
 
   app "GitFiend.app"
-  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
-  shimscript = "#{staged_path}/gitfiend.wrapper.sh"
-  binary shimscript, target: "gitfiend"
 
-  preflight do
-    File.write shimscript, <<~EOS
-      #!/bin/sh
-      exec '#{appdir}/GitFiend.app/Contents/MacOS/GitFiend' "$@"
-    EOS
-  end
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+
+  command_wrapper "gitfiend.wrapper.sh",
+                  executable: "#{appdir}/GitFiend.app/Contents/MacOS/GitFiend"
 
   zap trash: [
     "~/Library/Application Support/GitFiend",
